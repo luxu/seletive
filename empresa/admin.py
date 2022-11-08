26c3c8c9572d6e5fa6551087ed5359db622b9756
Luciano Martins <zicadopv@gmail.com>
2022-11-08 07:26:45 -0300

    Att

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from empresa.models import Tecnologias, Empresa, Vagas
+
+admin.site.register(Tecnologias)
+admin.site.register(Empresa)
+admin.site.register(Vagas)
--- empresa/views.py
+++ empresa/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from empresa.models import Tecnologias, Empresa
+
 
 def nova_empresa(request):
+    if request.method == "GET":
+        techs = Tecnologias.objects.all()
+        context = {
+            'techs': techs
+        }
+    else:
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        cidade = request.POST.get('cidade')
+        endereco = request.POST.get('endereco')
+        nicho = request.POST.get('nicho')
+        caracteristicas = request.POST.get('caracteristicas')
+        tecnologias = request.POST.getlist('tecnologias')
+        logo = request.FILES.get('logo')
+        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(
+                endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)):
+            # messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/home/nova_empresa')
+
+        if logo.size > 100_000_000:
+            # messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB')
+            return redirect('/home/nova_empresa')
+
+        if nicho not in [i[0] for i in Empresa.choices_nicho_mercado]:
+            # messages.add_message(request, constants.ERROR, 'Nicho de mercado inválido')
+            return redirect('/home/nova_empresa')
+        return HttpResponse("Tô aqui")
     template_name = 'empresa/nova_empresa.html'
-    return render(request, template_name)
+    return render(request, template_name, context)
