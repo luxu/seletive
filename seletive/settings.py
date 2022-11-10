4a54373219d8e4f4ff122aed9bc1fc4b247045bc
Luciano Martins <zicadopv@gmail.com>
2022-11-10 10:53:23 -0300

    Add whitenoise no settings.py

--- seletive/settings.py
+++ seletive/settings.py
@@ -91,10 +91,14 @@ TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "templates/static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+# STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
